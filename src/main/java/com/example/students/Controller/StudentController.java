d79a1a3f34cca638ae4ae46092aa9d23e02f1898
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-20 13:31:13 +0200

    Initialied Autowired

--- src/main/java/com/example/students/Controller/StudentController.java
+++ src/main/java/com/example/students/Controller/StudentController.java
@@ -1,9 +1,16 @@
 package com.example.students.Controller;
 
 
+import com.example.students.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("students") //This is the endpoint
 public class StudentController {
+
+    @Autowired
+    StudentRepository repos; //Repos can now be called to use object
 
 }
