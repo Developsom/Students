b274e6cbac918a78f850ef5c4609e317b1a76444
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-20 13:43:36 +0200

    Stuck and i dont know why

--- src/main/java/com/example/students/Controller/StudentController.java
+++ src/main/java/com/example/students/Controller/StudentController.java
@@ -5,9 +5,7 @@ import com.example.students.Entity.Student;
 import com.example.students.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("students") //This is the endpoint
@@ -19,6 +17,11 @@ public class StudentController {
     @GetMapping
     public ResponseEntity<Iterable<Student>> getStudents() {
         return ResponseEntity.ok(repos.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(RequestBody Student stud) {
+        repos.save(stud);
     }
 
 }
