ad84e09659b3d4a9d964bab6575049d2165f604f
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-20 13:26:36 +0200

    Declared Id Told SB to generate the id on its own Added datatype for id

--- src/main/java/com/example/students/Entity/Student.java
+++ src/main/java/com/example/students/Entity/Student.java
@@ -2,10 +2,18 @@ package com.example.students.Entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue
+    Long id;
+
+
+
 
 }
